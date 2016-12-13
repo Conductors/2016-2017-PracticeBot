1d02745e2c7b392b3eff138c2210b60fa7f01dce
Levi Pinkard <levipinkard@gmail.com>
2016-12-12 22:20:59 -0500

    Actually did what the last commit said

--- src/org/usfirst/frc/team4580/robot/Robot.java
+++ src/org/usfirst/frc/team4580/robot/Robot.java
@@ -1,9 +1,11 @@
 package org.usfirst.frc.team4580.robot;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -13,17 +15,38 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
  * directory.
  */
 public class Robot extends IterativeRobot {
+	//Need to declare Talons using CANTalon data type ASAP
+	CameraServer camera;
 	RobotDrive myRobot;
 	Joystick stick;
 	int autoLoopCounter;
-	
+    boolean joystickA;
+	boolean joystickB;
+	boolean joystickX;
+	boolean joystickY;
+	boolean joystickLSB;
+	boolean joystickRSB;
+	boolean joystickLB;
+	boolean joystickRB;
+	double joystickLSX;
+	double joystickLSY;
+	double joystickRSX;
+	double joystickRSY;
+	double joyLeftOut;;
+	double joyRightOut;
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
+    	//Sets left motor to channel #0, right motor to #1
     	myRobot = new RobotDrive(0,1);
+    	//Sets stick equal to joystick at port #0
     	stick = new Joystick(0);
+        //Sets up camera
+    	camera = CameraServer.getInstance();
+        camera.startAutomaticCapture("cam0");
+    	camera.setQuality(100);
     }
     
     /**
@@ -56,17 +79,11 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        myRobot.arcadeDrive(stick);
-    	/* Stick Init */
-        boolean joystickA = stick.getRawButton(2);
-    	boolean joystickB = stick.getRawButton(3);
-    	boolean joystickX = stick.getRawButton(1)
-    	boolean joystickY = stick.getRawButton(4)
-    	boolean joystickLSB = stick.getRawButton(11)
-    	boolean joystickRSB = stick.getRawButton(12)
-    	boolean joystickLB = stick.getRawButton(5)
-    	boolean joystickRB = stick.getRawButton(6)
-    	boolean
+    	//Refreshes input
+    	updateInput();
+        //Displays input values to smart dashboard
+    	smartDashboard();
+        driveBot();
     }
     
     /**
@@ -74,8 +91,32 @@ public class Robot extends IterativeRobot {
      */
     public void testPeriodic() {
     	LiveWindow.run();
-
-    	
     }
-    
+   public void driveBot() {
+	   // Sets tank drive equal to joystick values
+	   //May have to set to negatives, double check in dashboard
+	   joyLeftOut = joystickRSY;
+	   joyRightOut = joystickRSY;
+	   myRobot.tankDrive(joyLeftOut, joyRightOut, true);
+
+   }
+    public void updateInput() {
+        //Refreshes values from joystick and later, sensors
+    	joystickA = stick.getRawButton(2);
+        joystickB = stick.getRawButton(3);
+    	joystickX = stick.getRawButton(1);
+    	joystickY = stick.getRawButton(4);
+    	joystickLSB = stick.getRawButton(11);
+    	joystickRSB = stick.getRawButton(12);
+    	joystickLB = stick.getRawButton(5);
+    	joystickRB = stick.getRawButton(6);
+    	joystickLSX = stick.getRawAxis(0);
+    	joystickLSY = stick.getRawAxis(1);
+    	joystickRSX = stick.getRawAxis(2);
+    	joystickRSY = stick.getRawAxis(3);
+    }
+    public void smartDashboard(){
+        SmartDashboard.putNumber("Left Stick", joystickLSY);
+		SmartDashboard.putNumber("Right Stick", joystickRSY);
+    }
 }
